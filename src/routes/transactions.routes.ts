d3f6f5f1226ca5126bce70761abcfe5280c12815
Marcelo dos Santos <profmds@gmail.com>
2020-04-15 23:57:51 -0300

    Migrations Commit

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -8,11 +8,39 @@ import { Router } from 'express';
 const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
-  // TODO
+  try {
+    const transactions = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
+    const printTransaction = {
+      transactions,
+      balance,
+    };
+
+    return response.json(printTransaction);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
 });
 
 transactionsRouter.post('/', async (request, response) => {
-  // TODO
+  const { title, value, type, category } = request.body;
+
+  // criando o objeto de criação da transaçãod com o conceito de Dependency Invertion
+  // para que sempre a lista de transações abertas e não novas criadas
+  const createTransaction = new CreateTransactionService(
+    transactionsRepository,
+  );
+
+  // chamada da execução do servço para que os dados da transação sejam deveras adicionado
+  // no array de transações, para que o principío de cada arquivo realizar apenas seu papel
+  // manipulações de dados serão enviadas aos serviços, porém adicionadas através do repositório
+  const transaction = createTransaction.execute({
+    title,
+    value,
+    type,
+  });
+
+  return response.json(transaction);
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
